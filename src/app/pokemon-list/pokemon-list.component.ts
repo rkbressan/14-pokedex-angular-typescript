7ff811c2e948b964ada8281e711b15de29a5765f
Ricardo Bressan <wkbressan@gmail.com>
2022-11-23 09:49:19 -0300

    backup
    margin pokemon-card changed
    pokedex att 1 -> 100

--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -434,6 +434,428 @@ export class PokemonListComponent {
         Type.Ground,
       ],
     },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/051.png',
+      number: 51,
+      name: 'Dugtrio',
+      types: [
+        Type.Ground,
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/052.png',
+      number: 52,
+      name: 'Meowth',
+      types: [
+        Type.Normal,
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/053.png',
+      number: 53,
+      name: 'Persian',
+      types: [
+        Type.Normal,
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/054.png',
+      number: 54,
+      name: 'Psyduck',
+      types: [
+        Type.Water,
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/055.png',
+      number: 55,
+      name: 'Golduck',
+      types: [
+        Type.Water,
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/056.png',
+      number: 56,
+      name: 'Mankey',
+      types: [
+        Type.Fighting,
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/057.png',
+      number: 57,
+      name: 'Primeape',
+      types: [
+        Type.Fighting,
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/058.png',
+      number: 58,
+      name: 'Growlithe',
+      types: [
+        Type.Fire,
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/059.png',
+      number: 59,
+      name: 'Arcanine',
+      types: [
+        Type.Fire,
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/060.png',
+      number: 60,
+      name: 'Poliwag',
+      types: [
+        Type.Water,
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/061.png',
+      number: 61,
+      name: 'Poliwhirl',
+      types: [
+        Type.Water,
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/062.png',
+      number: 62,
+      name: 'Poliwrath',
+      types: [
+        Type.Water,
+        Type.Fighting,
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/063.png',
+      number: 63,
+      name: 'Abra',
+      types: [
+        Type.Psychic,
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/064.png',
+      number: 64,
+      name: 'Kadabra',
+      types: [
+        Type.Psychic,
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/065.png',
+      number: 65,
+      name: 'Alakazam',
+      types: [
+        Type.Psychic,
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/066.png',
+      number: 66,
+      name: 'Machop',
+      types: [
+        Type.Fighting,
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/067.png',
+      number: 67,
+      name: 'Machoke',
+      types: [
+        Type.Fighting,
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/068.png',
+      number: 68,
+      name: 'Machamp',
+      types: [
+        Type.Fighting,
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/069.png',
+      number: 69,
+      name: 'Bellsprout',
+      types: [
+        Type.Grass,
+        Type.Poison,
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/070.png',
+      number: 70,
+      name: 'Weepinbell',
+      types: [
+        Type.Grass,
+        Type.Poison,
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/071.png',
+      number: 71,
+      name: 'Victreebel',
+      types: [
+        Type.Grass,
+        Type.Poison,
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/072.png',
+      number: 72,
+      name: 'Tentacool',
+      types: [
+        Type.Water,
+        Type.Poison,
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/073.png',
+      number: 73,
+      name: 'Tentacruel',
+      types: [
+        Type.Water,
+        Type.Poison,
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/074.png',
+      number: 74,
+      name: 'Geodude',
+      types: [
+        Type.Rock,
+        Type.Ground,
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/075.png',
+      number: 75,
+      name: 'Graveler',
+      types: [
+        Type.Rock,
+        Type.Ground,
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/076.png',
+      number: 76,
+      name: 'Golem',
+      types: [
+        Type.Rock,
+        Type.Ground,
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/077.png',
+      number: 77,
+      name: 'Ponyta',
+      types: [
+        Type.Fire,
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/078.png',
+      number: 78,
+      name: 'Rapidash',
+      types: [
+        Type.Fire,
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/079.png',
+      number: 79,
+      name: 'Slowpoke',
+      types: [
+        Type.Water,
+        Type.Psychic,
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/080.png',
+      number: 80,
+      name: 'Slowbro',
+      types: [
+        Type.Water,
+        Type.Psychic,
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/081.png',
+      number: 81,
+      name: 'Magnemite',
+      types: [
+        Type.Electric,
+        Type.Steel,
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/082.png',
+      number: 82,
+      name: 'Magneton',
+      types: [
+        Type.Electric,
+        Type.Steel,
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/083.png',
+      number: 83,
+      name: 'Farfetch´d',
+      types: [
+        Type.Normal,
+        Type.Flying,
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/084.png',
+      number: 84,
+      name: 'Doduo',
+      types: [
+        Type.Normal,
+        Type.Flying,
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/085.png',
+      number: 85,
+      name: 'Dodrio',
+      types: [
+        Type.Normal,
+        Type.Flying,
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/086.png',
+      number: 86,
+      name: 'Seel',
+      types: [
+        Type.Water,
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/087.png',
+      number: 87,
+      name: 'Dewgong',
+      types: [
+        Type.Water,
+        Type.Ice,
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/088.png',
+      number: 88,
+      name: 'Grimer',
+      types: [
+        Type.Poison,
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/089.png',
+      number: 89,
+      name: 'Muk',
+      types: [
+        Type.Poison,
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/090.png',
+      number: 90,
+      name: 'Shellder',
+      types: [
+        Type.Water,
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/091.png',
+      number: 91,
+      name: 'Cloyster',
+      types: [
+        Type.Water,
+        Type.Ice,
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/092.png',
+      number: 92,
+      name: 'Gastly',
+      types: [
+        Type.Ghost,
+        Type.Poison,
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/093.png',
+      number: 93,
+      name: 'Haunter',
+      types: [
+        Type.Ghost,
+        Type.Poison,
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/094.png',
+      number: 94,
+      name: 'Gengar',
+      types: [
+        Type.Ghost,
+        Type.Poison,
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/095.png',
+      number: 95,
+      name: 'Onix',
+      types: [
+        Type.Rock,
+        Type.Ground,
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/096.png',
+      number: 96,
+      name: 'Drowzee',
+      types: [
+        Type.Psychic,
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/097.png',
+      number: 97,
+      name: 'Hypno',
+      types: [
+        Type.Psychic,
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/098.png',
+      number: 98,
+      name: 'Krabby',
+      types: [
+        Type.Water,
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/099.png',
+      number: 99,
+      name: 'Kingler',
+      types: [
+        Type.Water,
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/100.png',
+      number: 100,
+      name: 'Voltorb',
+      types: [
+        Type.Electric,
+      ],
+    },
   ];
 }
 
